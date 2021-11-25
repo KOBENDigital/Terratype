688e557ef841171359afe53244bbb3ceef8fb921
Omar <omar.alkylaney@gmail.com>
2021-11-25 13:48:46 +1000

    Update the Map Converter to inherit from IPropertyValueConverter instead of  PropertyValueConverterBase

--- src/Core/Terratype/DataEditors/Map/MapConverter.cs
+++ src/Core/Terratype/DataEditors/Map/MapConverter.cs
@@ -11,7 +11,7 @@ namespace Terratype.DataEditors.Map
 	//  CurrentPage.Map.Position.ToString()
 	//  CurrentPage.Map.Zoom;
 
-	public class MapConverter : PropertyValueConverterBase
+	public class MapConverter : IPropertyValueConverter
 	{
 		IDataTypeService DataTypeService;
 
@@ -20,47 +20,61 @@ namespace Terratype.DataEditors.Map
 			DataTypeService = dataTypeService;
 		}
 
-		public override bool IsConverter(IPublishedPropertyType propertyType) => propertyType.EditorAlias == MapDataEditor.DataEditorAlias;
-		public override PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType) => PropertyCacheLevel.Element;
 
-		public override Type GetPropertyValueType(IPublishedPropertyType propertyType) => typeof(IMap);
+    public bool IsConverter(IPublishedPropertyType propertyType) => propertyType.EditorAlias == MapDataEditor.DataEditorAlias;
+    public Type GetPropertyValueType(IPublishedPropertyType propertyType) => typeof(IMap);
+    public PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType) => PropertyCacheLevel.Element;
+
+		public bool? IsValue(object value, PropertyValueLevel level)
+    {
+      if (level == PropertyValueLevel.Source)
+        return value != null && (!(value is string val) || string.IsNullOrWhiteSpace(val) == false);
+      throw new NotSupportedException($"Invalid level: {level}.");
+    }
+		
+
+		public object ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object source,
+      bool preview)
+    {
+      if (source == null) return null;
+
+
+			var values = DataTypeService.GetDataType(propertyType.EditorAlias);
+      var config = JObject.Parse((string)values.Configuration);
+
+      JObject data;
+      if (source is string val && !string.IsNullOrWhiteSpace(val))
+      {
+        data = JObject.Parse(val);
+      }
+      else
+      {
+        data = new JObject();
+        MergeJson(data, config, nameof(IMap.Zoom));
+        MergeJson(data, config, nameof(IMap.Position));
+      }
+      var innerConfig = config.GetValue("config") as JObject;
+      MergeJson(data, innerConfig, nameof(IMap.Icon));
+      MergeJson(data, innerConfig, nameof(IMap.Provider));
+      MergeJson(data, innerConfig, nameof(IMap.Height));
+      return new Terratype.Map(data);
+		}
+
+    public object ConvertIntermediateToObject(IPublishedElement owner, IPublishedPropertyType propertyType,
+      PropertyCacheLevel referenceCacheLevel, object inter, bool preview)
+    {
+      return inter;
+    }
+
+    public object ConvertIntermediateToXPath(IPublishedElement owner, IPublishedPropertyType propertyType,
+      PropertyCacheLevel referenceCacheLevel, object inter, bool preview)
+    {
+      return null;
+    }
+
 
 		private void MergeJson(JObject data, JObject config, string fieldName) => 
 			data.Merge(new JObject(new JProperty(Json.PropertyName<IMap>(fieldName), config.GetValue(Json.PropertyName<IMap>(fieldName), StringComparison.InvariantCultureIgnoreCase))));
-
-		public object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
-		{
-			var values = DataTypeService.GetDataType(propertyType.EditorAlias);
-			var config = JObject.Parse((string) values.Configuration);
-
-			JObject data = null;
-			if (source is string && !string.IsNullOrWhiteSpace((string) source))
-			{
-				data = JObject.Parse((string)source);
-			}
-			else
-			{
-				data = new JObject();
-				//MergeJson(data, config, nameof(Models.Map.Lookup));
-				MergeJson(data, config, nameof(IMap.Zoom));
-				MergeJson(data, config, nameof(IMap.Position));
-			}
-			var innerConfig = config.GetValue("config") as JObject;
-			MergeJson(data, innerConfig, nameof(IMap.Icon));
-			MergeJson(data, innerConfig, nameof(IMap.Provider));
-			MergeJson(data, innerConfig, nameof(IMap.Height));
-			return new Terratype.Map(data);
-		}
-
-		public object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview)
-		{
-			return source;
-		}
-
-		public object ConvertSourceToXPath(PublishedPropertyType propertyType, object source, bool preview)
-		{
-			return null;
-		}
-	}
+  }
 }
 
